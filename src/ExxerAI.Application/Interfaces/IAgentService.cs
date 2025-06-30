b333f038b1916447fdb20239f7a82417844f1008
hebelmx <hebelmx@gmail.com>
2025-06-30 00:05:49 -0600

    deleted all solution

--- src/ExxerAI.Application/Interfaces/IAgentService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExxerAI.Application.Interfaces
-{
-    public interface IAgentService
-    {
-        Task ExecuteAgentTaskAsync(string prompt, string agentType);
-
-        Task ExecuteTaskAsync(string prompt, string agentType);
-    }
-}
--- src/ExxerAI.WebAPI/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Compiler", "CS1591:Missing XML comment for publicly visible type or member", Justification = "<Pending>")]
