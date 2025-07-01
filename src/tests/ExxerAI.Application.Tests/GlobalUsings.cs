e632b9eeca2ea82c298122058fa781c94d34d86a
hebelmx <hebelmx@gmail.com>
2025-07-01 08:29:35 -0600

    Claude Refactoring

---
+++ src/tests/ExxerAI.Application.Tests/GlobalUsings.cs
@@ -0,0 +1,12 @@
+// Global using statements for ExxerAI Application Tests
+global using Xunit;
+global using NSubstitute;
+global using Shouldly;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using ExxerAI.Application.Interfaces;
+global using ExxerAI.Application.Services;
+global using ExxerAI.Domain; 
---
+++ src/tests/ExxerAI.Orchestration.Tests/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using statements for ExxerAI Orchestration Tests
+global using Xunit;
+global using NSubstitute;
+global using Shouldly;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using ExxerAI.Orchestration;
+global using ExxerAI.Domain; 
