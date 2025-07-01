cf2195f254112c828f7c0b5e3c72b5222d59e206
hebelmx <hebelmx@gmail.com>
2025-07-01 11:27:28 -0600

    compiling and resolving packages

--- src/ExxerAi.MCPServer/Program.cs
+++ src/ExxerAi.MCPServer/Program.cs
@@ -6,6 +6,7 @@ using ExxerAi.MCPServer.Application.Tools;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
 using MudBlazor.Services;
 using Serilog;
 using ModelContextProtocol.AspNetCore;
